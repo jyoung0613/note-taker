f74859f8fe11c6a03d11a2c0c87cb164f880e465
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-10-03 12:27:30 -0600

    added server js file

--- server.js
+++
@@ -1,9 +0,0 @@
-// Dependencies
-const express = require('express');
-const apiRoutes = require('./routes/apiRoutes')
-const htmlRoutes = require('./routes/htmlRoutes')
-
-const app = express();
-
-// Port
-const PORT = process.env.PORT || 3000;
