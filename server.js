2665695dac3676f115e58ec15b59f86bb6a5ecbd
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-10-03 13:53:30 -0600

    seems to be working

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-// Dependencies
+// DEPENDENCIES
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes')
 const htmlRoutes = require('./routes/htmlRoutes')
-const deleteRoutes = require('./routes/deleteRoutes') 
 
 const app = express();
 
-// Port
+//PORT
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
 
-// Router
-app.use('/routes', apiRoutes)
-app.use('/routes', htmlRoutes)
-app.use('/routes', deleteRoutes)
+// ROUTER
+app.use('/api', apiRoutes)
+app.use('/', htmlRoutes)
 
-// Listener
+// LISTENER
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
+  console.log(`App listening on PORT: ${PORT}`);
 });
